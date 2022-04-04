48e10ca48eeeda6065abd594df41d4c0d762c23a
monolith <30329959+vorstoffel@users.noreply.github.com>
2022-04-04 02:51:21 +0200

    renaming

--- angular-app/src/app/app.component.spec.ts
+++ angular-app/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'didactic-happiness-app'`, () => {
+  it(`should have as title 'angular-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('didactic-happiness-app');
+    expect(app.title).toEqual('angular-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('didactic-happiness-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-app app is running!');
   });
 });
